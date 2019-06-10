6b05c3191fef71ec254b9d8ab05a94fc0a3ceb8d
Michael Scherer <mail@scherermichael.net>
2019-06-10 18:42:16 +0200

    .

--- scripts/github.js
+++ scripts/github.js
@@ -13,7 +13,7 @@ module.exports = (robot) => {
         if (err) {
           res.reply(`Sorry, but the following error occured: ${err.message}`);
         } else {
-          res.reply(`Creation of repository ${repository} triggered. See #terraform-test for appoval notification.`);
+          res.reply(`Creation of repository ${repository} triggered. You'll get a request for approval in a miunte. See #terraform-test for the notification.`);
         }
       });
   });
